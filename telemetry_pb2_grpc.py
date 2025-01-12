ea968f77ae7e18a5a35dcf5b15a46515a6386b4a
Bastian Krohg <98895859+bastiankrohg@users.noreply.github.com>
2025-01-12 16:29:19 +0100

    Added local import

--- telemetry_pb2_grpc.py
+++ telemetry_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import telemetry_pb2 as telemetry__pb2
+from . import telemetry_pb2 as telemetry__pb2
 
 
 class TelemetryServiceStub(object):
